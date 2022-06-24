f5b0197eb671b7f2265529b62233d089536911d4
gaengsigi <elvin2@naver.com>
2022-06-24 15:01:45 +0900

    first commit

---
+++ application/controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+namespace application\controllers;
+
+require_once "application/utils/UrlUtils.php";
+
+class UserController extends Controller {
+    public function signin() {        
+        return "user/signin.php";
+    }
+
+    public function signup() {
+        return "user/signup.php";
+        $method = getMethod();
+        switch($method) {
+            case _GET:
+                return;
+            case _POST:
+                return;
+        }
+    }
+}
