75fa29e4eb75a3c07039b308d55c501e54fa1c89
gaengsigi <elvin2@naver.com>
2022-06-24 18:06:39 +0900

    6/24

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,21 +1,46 @@
 <?php
 namespace application\controllers;
 
-require_once "application/utils/UrlUtils.php";
-
 class UserController extends Controller {
     public function signin() {        
-        return "user/signin.php";
+        switch(getMethod()) {
+            case _GET:
+                return "user/signin.php";
+            case _POST:
+
+                $param = [
+                    "email" => $_POST["email"],
+                    "pw" => $_POST["pw"]
+                ];
+
+                $dbUser = $this->model->selUser($param);
+                
+                // 아이디, 비번이 하나라도 없거나 틀리면 > /user/signin 리다이렉트
+                if(!$dbUser || !password_verify($param["pw"], $dbUser->pw)) {
+                    return "redirect:signin?email={$param["email"]}&err";
+                }
+
+                // 아이디, 비번이 맞다면 > /feed/index 리다이렉트 해주세요.
+                return "redirect:/feed/index";
+            }
     }
 
     public function signup() {
-        return "user/signup.php";
-        $method = getMethod();
-        switch($method) {
+        switch(getMethod()) {
             case _GET:
-                return;
+                return "user/signup.php";
             case _POST:
-                return;
+                $param = [
+                    "email" => $_POST["email"],
+                    "pw" => $_POST["pw"],
+                    "nm" => $_POST["nm"]
+                ];
+
+                // 비밀번호 암호화
+                $param["pw"] = password_hash($param["pw"], PASSWORD_BCRYPT);
+                $this->model->insUser($param);
+
+                return "redirect:signin";
         }
-    }
+    } 
 }
