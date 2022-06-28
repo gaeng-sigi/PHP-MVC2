84ecad61e570d49ef3af2b090f0aaecb5c1f91dd
gaengsigi <elvin2@naver.com>
2022-06-28 15:23:42 +0900

    6/28

--- static/js/feed/index.js
+++ static/js/feed/index.js
@@ -1,12 +1,13 @@
 (function () {
-    const btnNewFeedModal = document.querySelector("#btnNewFeedModal"); // btnNewFeedModal 지역변수
+    const btnNewFeedModal = document.querySelector("#btnNewFeedModal");
     if (btnNewFeedModal) {
         const modal = document.querySelector("#newFeedModal");
         const body = modal.querySelector("#id-modal-body");
         const frmElem = modal.querySelector("form");
 
-        //이미지 값이 변하면
-        frmElem.imgs.addEventListener("change", function (e) { // .으로 자식한테 접근(form 태그만)
+    //이미지 값이 변하면
+        frmElem.imgs.addEventListener("change", function (e) {
+            // console.log(`length: ${e.target.files.length}`);
             if (e.target.files.length > 0) {
                 body.innerHTML = `
                     <div>
@@ -37,25 +38,18 @@
 
                     const fData = new FormData();
                     for (let i = 0; i < files.length; i++) {
-                        fData.append("imgs", files[i]);
+                        fData.append("imgs[]", files[i]);
                     }
                     fData.append("ctnt", body.querySelector("textarea").value);
-                    fData.append(
-                    "location",
-                    body.querySelector("input[type=text]").value
-                    );
+                    fData.append("location", body.querySelector("input[type=text]").value);
 
-                fetch("/feed/reg", {
-                    method: "post",
-                    body: fData,
-                }).then((res) => res.json())
+                    fetch("/feed/rest", {
+                        method: "post",
+                        body: fData,
+                    })
+                    .then((res) => res.json())
                     .then((myJson) => {
-                        const closeBtn = modal.querySelector(".btn-close");
-                        closeBtn.click();
-
-                        if (feedObj && myJson.result) {
-                            feedObj.refreshList();
-                        }
+                        console.log(myJson);
                     });
                 });
             }
@@ -67,7 +61,7 @@
             selFromComBtn.className = "btn btn-primary";
             selFromComBtn.innerText = "컴퓨터에서 선택";
             selFromComBtn.addEventListener("click", function () {
-            frmElem.imgs.click();
+                frmElem.imgs.click();
             });
             body.innerHTML = null;
             body.appendChild(selFromComBtn);
