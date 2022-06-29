7f3a4e1b45d220a8d0e838954099835f2898bee8
gaengsigi <elvin2@naver.com>
2022-06-29 13:16:46 +0900

    6/29

--- static/js/common.js
+++ static/js/common.js
@@ -1,1 +1,10 @@
-
+function encodeQueryString(params) {
+    const keys = Object.keys(params);
+    
+    return keys.length
+        ? "?" + keys.map(key =>
+            encodeURIComponent(key) + "=" +
+            encodeURIComponent(params[key])
+        ).join("&")
+    :"";
+}
