6aaa65c357e1707e033b3e2dc21c960ad6b8bf38
gaengsigi <elvin2@naver.com>
2022-07-08 18:06:58 +0900

    7/8 ip추적

--- application/views/user/signup.php
+++ application/views/user/signup.php
@@ -1,3 +1,28 @@
+<?php
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if (getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if (getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if (getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if (getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if (getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+//echo "SERVER : ".$_SERVER['REMOTE_ADDR'];
+//echo "<br>";
+//echo "getenv : ".get_client_ip();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once "application/views/template/head.php"; ?>
