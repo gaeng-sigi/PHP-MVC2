6fc01f9fbc6a9a8790931cf8be2fe52e945b5474
gaengsigi <elvin2@naver.com>
2022-07-08 17:31:46 +0900

    ip 추적

--- application/views/user/signup.php
+++ application/views/user/signup.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once "application/views/template/head.php"; ?>
+
 <body>
     <div>
         <h1>회원가입</h1>
@@ -9,10 +10,12 @@
             <div><input type="email" name="email" placeholder="email" autofocus required></div>
             <div><input type="password" name="pw" placeholder="password" required></div>
             <div><input type="text" name="nm" placeholder="name" required></div>
+            <div><input type="hidden" name="uip" value="<?= $_SERVER['REMOTE_ADDR'] ?>" required></div>
             <div>
                 <input type="submit" value="회원가입">
             </div>
         </form>
     </div>
 </body>
+
 </html>
